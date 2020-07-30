c5159a94e083405f1837893ef881936e4c41eb10
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-07-30 00:23:43 -0300

    Creation of the logic of the fruit spawner.

---
+++ Assets/Scripts/FruitSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour
+{
+    public GameObject fruit;
+    public Transform[] spawnPoints;
+    public float minSpawnDelay = 0.1f;
+    public float maxSpawnDelay = 0.8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {}
+
+    IEnumerator Spawn()
+    {
+        while(true)
+        {
+            float timer = Random.Range(minSpawnDelay, maxSpawnDelay);
+            yield return new WaitForSeconds(1f);
+            int spawnIndex = Random.Range(0, spawnPoints.Length);
+            Transform spawnPoint = spawnPoints[spawnIndex];
+            GameObject spawnedFruit = Instantiate(fruit, spawnPoint.position, spawnPoint.rotation);
+            Destroy(spawnedFruit, 5f);
+        }
+    }
+}
