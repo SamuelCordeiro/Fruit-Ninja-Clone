6c4bdbf03b79019100da3787c571fdaeb5f2fb57
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-08-04 00:10:27 -0300

    Bomb upgrade.

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public float force;
+    private Rigidbody2D rig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //force = Random.Range(10f, 14f);
+        rig = GetComponent<Rigidbody2D>();
+        rig.AddForce(transform.up * force, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) 
+    {
+        if(collider.tag == "Blade")
+        {
+            GameController.instance.life--;
+            GameController.instance.LifeTextUpdate();
+            Destroy(gameObject);
+        }
+    }
+}
